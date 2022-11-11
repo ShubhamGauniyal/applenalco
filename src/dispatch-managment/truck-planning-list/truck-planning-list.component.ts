89e47ec6d4c1c177e3e4570ac7857ddbfbd5f9bf
ShubhamGauniyal <shubhamgauniyal10@gmail.com>
2022-11-11 16:20:16 +0530

    Truck Planning List

--- src/dispatch-managment/truck-planning-list/truck-planning-list.component.ts
+++ src/dispatch-managment/truck-planning-list/truck-planning-list.component.ts
@@ -16,10 +16,22 @@ export interface Element {
 }
 
 const ELEMENT_DATA: Element[] = [
-  {position: 1, deliverydate: 'Hydrogen', orderno: 1.0079, vendorcode: 'H',vendorname:'eska',route:'alaska',transporter:'ddl',status:'sent',doqty:1,
+  {position: 1, deliverydate: '15/11/2022', orderno: 1.0079, vendorcode: 'H',vendorname:'eska',route:'alaska',transporter:'ddl',status:'sent',doqty:1,
   diqty:2,
   balqty:3},
-  {position: 2, deliverydate: 'drogen', orderno: 1.79, vendorcode: 'G',vendorname:'albama',route:'albama',transporter:'ddc',status:'loading',doqty:1,
+  {position: 2, deliverydate: '16/11/2022', orderno: 1.79, vendorcode: 'G',vendorname:'albama',route:'albama',transporter:'ddc',status:'loading',doqty:1,
+  diqty:2,
+  balqty:3},
+  {position: 3, deliverydate: '19/11/2022', orderno: 1.0079, vendorcode: 'H',vendorname:'eska',route:'alaska',transporter:'ddl',status:'sent',doqty:1,
+  diqty:2,
+  balqty:3},
+  {position: 4, deliverydate: '18/11/2022', orderno: 1.79, vendorcode: 'G',vendorname:'albama',route:'albama',transporter:'ddc',status:'loading',doqty:1,
+  diqty:2,
+  balqty:3},
+  {position: 5, deliverydate: '16/11/2022', orderno: 1.0079, vendorcode: 'H',vendorname:'eska',route:'alaska',transporter:'ddl',status:'sent',doqty:1,
+  diqty:2,
+  balqty:3},
+  {position: 6, deliverydate: '15/11/2022', orderno: 1.79, vendorcode: 'G',vendorname:'albama',route:'albama',transporter:'ddc',status:'loading',doqty:1,
   diqty:2,
   balqty:3},
  
@@ -45,7 +57,9 @@ export class TruckPlanningListComponent implements OnInit {
   
   
   displayedColumns = ['position', 'deliverydate', 'orderno', 'vendorcode','route','transporter','status','doqty','diqty','balqty','select'];
-  dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
+  elementOriginalData = ELEMENT_DATA;
+  dataSource = new MatTableDataSource<Element>(this.elementOriginalData);
+selectedElement: Element[] = [];
 
   
   constructor(private route:Router) {
@@ -71,9 +85,20 @@ console.log(event,'myevent');
 
   truckPlanning(){
     this.modifyPlanning = true;
+    this.dataSource = new MatTableDataSource<Element>(this.selectedElement);
   }
 
 saveModify(){
   this.modifyPlanning = false;
+}
+
+selectedList(element: Element){
+  this.selectedElement.push(element);
+}
+
+closeButton(){
+  this.dataSource = new MatTableDataSource<Element>(this.elementOriginalData);
+  this.modifyPlanning = false;
+  this.selectedElement=[];
 }
 }
